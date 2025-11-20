ca736d7bf1c16930062771771c3ee74b566b6be7
maple <maplecai142857@gmail.com>
2025-11-20 04:55:48 -0500

    gitignore

--- server.py
+++ server.py
@@ -2,8 +2,7 @@ import os
 import sys
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
-ROOT_DIR = BASE_DIR.parent
+ROOT_DIR = Path(__file__).resolve().parent
 sys.path.append(str(ROOT_DIR))
 from MPRA_predict import models, datasets, metrics, utils
 from MPRA_predict.utils import *
@@ -17,18 +16,18 @@ assays = df_track.columns.tolist()
 
 
 subprocess.run([
-    "python", "0_predict_Sei_feature.py", 
-    "-i", "../data/Random/random_200bp_seqs.csv",
-    "-o", "../data/Random/random_200bp_Sei_pred.npy"])
+    "python", "predict_CRE_activity/0_predict_Sei_feature.py", 
+    "-i", "./data/Random/random_200bp_seqs.csv",
+    "-o", "./data/Random/random_200bp_Sei_pred.npy"])
 
 subprocess.run([
-    "python", "0_predict_Sei_VEF.py", 
-    "-i", "../data/Random/random_200bp_Sei_pred.npy",
-    "-o", "../data/Random/random_200bp_Sei_VEF.tsv"])
+    "python", "predict_CRE_activity/0_predict_Sei_VEF.py", 
+    "-i", "./data/Random/random_200bp_Sei_pred.npy",
+    "-o", "./data/Random/random_200bp_Sei_VEF.tsv"])
 
 subprocess.run([
-    "python", "0_predict_EpiCast_activity.py", 
-    "--config", "../configs/config_1120_test.yaml",
-    "--total_dataset.args.seq_file_path", "../data/Random/random_200bp_seqs.csv", 
-    "--total_dataset.args.epi_file_path", "../data/Random/random_200bp_Sei_VEF.tsv",
+    "python", "predict_CRE_activity/0_predict_EpiCast_activity.py", 
+    "--config", "./configs/config_1120_test.yaml",
+    "--total_dataset.args.seq_file_path", "./data/Random/random_200bp_seqs.csv", 
+    "--total_dataset.args.epi_file_path", "./data/Random/random_200bp_Sei_VEF.tsv",
     "--output_name", "random_200bp_EpiCast_pred.npy"])
